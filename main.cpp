8061cab8f9838f1a0341791fb7847d13338cfd51
IhabRagabMansour <68652696+IhabRagabMansour@users.noreply.github.com>
2023-11-03 07:32:00 +0200

    Added getAverage function

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 int getSum(const vector<int> &vec);
+float getAvrage(const vector<int> &vec);
 
 int main(){ 
 
 vector<int> vec={1,2,3,4,5,6,7,8,9,10};
 int sum = getSum(vec);
 cout<<"The sum of the Vector = "<<sum<<endl;
+int avg = getAvrage(vec);
+cout<<"The average of the Vector = "<<avg<<endl;
 
 return 0;
 }
@@ -20,4 +23,12 @@ int getSum(const vector<int> &vec){
         sum+=number;
     }
     return sum;
+}
+
+float getAvrage(const vector<int> &vec){
+
+    int sum=getSum(vec);
+    float avg=(float)sum/vec.size();
+
+    return avg;
 }
